b79c11a47fe438304e3d7ba73ae1dfbc66086ad2
Tomasz Suski <tomasz.suski@orba.co>
2024-03-11 09:54:28 +0100

    exercise 4 refactoring

--- src/test/java/com/lingarogrup/exercise4/Exercise4Test.java
+++ src/test/java/com/lingarogrup/exercise4/Exercise4Test.java
@@ -9,8 +9,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Exercise4Test {
     @Test
-    public void isTheMethodreturningRightLeterOrdinal(){
-        List letters = Arrays.asList(new String[] {"a", "z", "w", "f", "c", "h"});
+    public void isTheMethodReturningRightLeterOrdinal(){
+        List<String> letters = Arrays.asList("a", "z", "w", "f", "c", "h");
         int[] expectedOutputs = {1, 26, 23, 6, 3, 8};
         for (Object letter: letters) {
             int letterOrdinal = Exercise4.returnLetterOrderInAlphabet(letter.toString());
